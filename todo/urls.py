6407584f8330cde86af4e4b33a7a1b4f8f4dae16
abaj1d777 <amuhammadov719@gmail.com>
2022-07-12 12:45:45 +0500

    Todo loixasi

--- todo/urls.py
+++ todo/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from asosiy.view import *
+
+from todo.asosiy.views import TodoView
+
 router = DefaultRouter
-router.register('',TodoView)
+router.register('Todo/',TodoView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
